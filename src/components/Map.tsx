f806b131b6231a5fed179370a9aec88943a9c95a
veronika <vesparragozam@gmail.com>
2022-12-02 15:07:21 +0100

    Feature/Starting project with dependencies

---
+++ src/components/Map.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { ComposableMap, Geographies, Geography } from "react-simple-maps"
+
+const geoUrl =
+  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
+
+export default function Map() {
+  return (
+
+    //TODO: cambiar el color con Fill
+    
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) =>
+          geographies.map((geo) => (
+            <Geography key={geo.rsmKey} geography={geo} />
+          ))
+        }
+      </Geographies>
+    </ComposableMap>
+  )
+}
---
+++ src/views/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DatePicker from '../components/DatePicker'
+import Map from '../components/Map'
+import GenericCard from '../components/GenericCard'
+
+const Home = () => {
+  return (
+    <div>
+      <DatePicker/>
+      <Map/>
+      <GenericCard/>
+    </div>
+  )
+}
+
+export default Home
